a828a0ceec5aa3e8a424d27418d34cd5d7b75316
Lev Nagdimunov <levnagdimunov0@hotmail.com>
2021-05-16 00:39:58 -0400

    Add deprecation machinery

--- pds4_tools/utils/compat.py
+++ pds4_tools/utils/compat.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import unicode_literals
 
+import inspect
 from xml.etree import ElementTree as ET
 
 from ..extern import six
@@ -24,3 +25,23 @@ ET_Element = ET.Element if isinstance(ET.Element, six.class_types) else ET._Elem
 ET_Tree_iter = ET.ElementTree.iter if hasattr(ET.ElementTree, 'iter') else ET.ElementTree.getiterator
 ET_Element_iter = ET_Element.iter if hasattr(ET_Element, 'iter') else ET_Element.getiterator
 ET_ParseError = ET.ParseError if hasattr(ET, 'ParseError') else None
+
+
+# signature.bind(...).arguments compat (Python 3.3+)
+def bind_arguments(func, *args, **kwargs):
+    # Python 3.3+
+    try:
+        signature = inspect.signature(func)
+        arguments = signature.bind(*args, **kwargs).arguments
+    except AttributeError:
+        # Python 2.7+
+        try:
+            arguments = inspect.getcallargs(func, *args, **kwargs)
+            defaults = inspect.getcallargs(func, (), ())
+            for arg in arguments.keys():
+                if (defaults[arg] == arguments[arg]) and (arg not in kwargs):
+                    del arguments[arg]
+        except AttributeError:
+            arguments = kwargs
+
+    return arguments
