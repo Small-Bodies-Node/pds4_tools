26864efff1915e16983689324fa8e59ccde409d0
Lev Nagdimunov <levnagdimunov0@hotmail.com>
2021-10-10 13:13:34 -0400

    Update version to v1.3

--- pds4_tools/__about__.py
+++ pds4_tools/__about__.py
@@ -1,5 +1,5 @@
 __author__ = "Lev Nagdimunov"
-__copyright__ = "2015 - 2020, University of Maryland"
+__copyright__ = "2015 - 2021, University of Maryland"
 
-__version__ = "1.3.dev0"
+__version__ = "1.3"
 __email__ = "lnagdi1@astro.umd.edu"
