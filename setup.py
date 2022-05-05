50df4401897131fcccd37f5758d31a361ff34b37
Lev Nagdimunov <levnagdimunov0@hotmail.com>
2022-05-05 18:06:44 -0400

    Add testing and official support for Python 3.10

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 
     packages=find_packages(exclude=['doc', '*.tests', '*.tests.*']),
