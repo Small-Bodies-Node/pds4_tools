22c33a31d139df4e0c448625885c670b3c26f5dc
Lev Nagdimunov <levnagdimunov0@hotmail.com>
2023-01-23 23:36:35 -0500

    Fix pytest deprecation warnings for use of ``cls.setup``

--- pds4_tools/tests/__init__.py
+++ pds4_tools/tests/__init__.py
@@ -9,19 +9,22 @@ from posixpath import join as urljoin
 
 class PDS4ToolsTestCase(object):
 
-    def setup(self):
+    # Path to local data directory
+    local_data_dir = os.path.join(os.path.dirname(__file__), 'data')
 
-        self.local_data_dir = os.path.join(os.path.dirname(__file__), 'data')
-        self.web_data_dir = 'https://raw.githubusercontent.com/Small-Bodies-Node/pds4_tools/master/pds4_tools/tests/data/'
+    # Path to remote data directory
+    web_data_dir = 'https://raw.githubusercontent.com/Small-Bodies-Node/pds4_tools/master/pds4_tools/tests/data/'
 
-    def data(self, filename, from_web=False):
+    # Retrieve data locally or from web
+    @classmethod
+    def data(cls, filename, from_web=False):
 
         if from_web:
             path_join_func = urljoin
-            data_dir = self.web_data_dir
+            data_dir = cls.web_data_dir
 
         else:
             path_join_func = os.path.join
-            data_dir = self.local_data_dir
+            data_dir = cls.local_data_dir
 
         return path_join_func(data_dir, filename)
