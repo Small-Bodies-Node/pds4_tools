686ca0b31628f6d7ba40a54da9626a90cb3a33a7
Lev Nagdimunov <levnagdimunov0@hotmail.com>
2026-02-14 15:27:20 -0500

    Drop support for Python 3.5

--- setup.py
+++ setup.py
@@ -64,7 +64,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -80,7 +79,7 @@ setup(
 
     zip_safe=False,
 
-    python_requires='>= 2.7, != 3.0.*, != 3.1.*, != 3.2.*, != 3.3.*, != 3.4.*',
+    python_requires='>= 2.7, != 3.0.*, != 3.1.*, != 3.2.*, != 3.3.*, != 3.4.*, != 3.5.*',
 
     install_requires=[
         'numpy',
